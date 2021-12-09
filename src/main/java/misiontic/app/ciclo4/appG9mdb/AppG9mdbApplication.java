ac2a749669c696ed11c601d2efc7cc3c869dfcd9
JhostinA <jhostin.avendano.mt@correo.usa.edu.co>
2021-12-09 14:13:09 -0500

    subiendo reto 3

---
+++ src/main/java/misiontic/app/ciclo4/appG9mdb/AppG9mdbApplication.java
@@ -0,0 +1,13 @@
+package misiontic.app.ciclo4.appG9mdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppG9mdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppG9mdbApplication.class, args);
+	}
+
+}
